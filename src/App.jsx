f665da010b8fafaa3886f8733f1d7f4b21b22995
Dale Seo <5466341+DaleSeo@users.noreply.github.com>
2024-12-28 20:22:16 +0000

    Init Vite React

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          카운트는 {count} 입니다.
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
